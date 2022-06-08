c2a5ef41b98da499e965d17cd7b52e97b13ce26d
Mungunshagai <mungunshagai@coinhub.mn>
2022-06-08 10:56:37 +0800

    first commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x38DEE49EC3e5c3f41B892090Cf6dC4bf85dd5E9e";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
